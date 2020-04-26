0ca16fd536e7028cab78eca7688cc9488c17678f
da-pawanrat <s6130613051@phuket.psu.ac.th>
2020-04-27 02:43:49 +0700

    Enter

---
+++ thailand.js
@@ -0,0 +1,39 @@
+$(function () {
+        let Confirmed = document.getElementById('Confirmed');
+        let Death = document.getElementById('Death');
+        let Hospitalized = document.getElementById('hospital');
+        let Recovered = document.getElementById('Recovered');
+        
+        fetch("https://covid19.th-stat.com/api/open/today")
+
+        .then(response => response.json().then(data => {
+            console.log(data);
+            Confirmed.innerHTML = data.Confirmed;
+            Hospitalized.innerHTML = data.Hospitalized;
+            Death.innerHTML = data.Deaths;
+            Recovered.innerHTML = data.Recovered;
+        
+        }))
+        
+        .catch(err => {
+            console.log(err);
+        });
+    })
+$(function(){
+    let provinced = document.getElementById('provinces')
+     
+    fetch("https://covid19.th-stat.com/api/open/cases/sum")
+    .then(response => response.json().then(data =>{
+        console.log(data);
+        data.forEach(data => {
+        provinced.innerHTML = `<tr>
+        <th scope="provinced">${data.province}</th>
+        <td>${data.province}</td>
+        <td>${data.province.value}</td>
+
+      </tr>`; });
+        
+        }))
+        
+})
+
